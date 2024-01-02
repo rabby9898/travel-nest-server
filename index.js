483ac10ce9f7aeaaa17d4140594c79059c7685b0
rabby9898 <mdfajlerabby00@gmail.com>
2024-01-02 21:09:08 +0600

    become host api added

--- index.js
+++ index.js
@@ -153,7 +153,18 @@ async function run() {
       const options = { upsert: true };
       const isExist = await usersCollection.findOne(query);
       console.log("User found?----->", isExist);
-      if (isExist) return res.send(isExist);
+      if (isExist) {
+        if (user?.status === "Requested") {
+          const result = await usersCollection.updateOne(
+            query,
+            { $set: user },
+            options
+          );
+          return res.send(result);
+        }
+      } else {
+        return res.send(isExist);
+      }
       const result = await usersCollection.updateOne(
         query,
         {
